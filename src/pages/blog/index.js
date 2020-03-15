981b783086fb5eea4fee7fdabb957aa19369303d
kazukinakamuraKN <kajirikajiri@gmail.com>
2020-03-15 22:27:56 +0900

    fix

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -7,21 +7,22 @@ export default class BlogIndexPage extends React.Component {
   render() {
     return (
       <Layout>
-        <div
-          style={{display:'flex', justifyContent: 'start', paddingTop: 100}}
-        >
-          <h1
-            className="has-text-weight-bold is-size-1"
-            style={{
-              color: 'white',
-              padding: '1rem',
-            }}
-          >
-            Latest Stories
-          </h1>
-        </div>
         <section className="section">
           <div className="container">
+            <h1
+              className="has-text-weight-bold is-size-1"
+              style={{
+                display: 'flex',
+                justifyContent: 'start',
+                alignItems: 'center',
+                height: 150,
+                marginBottom: 20,
+                color: 'white',
+                padding: '1rem',
+              }}
+            >
+              Latest Stories
+            </h1>
             <div className="content">
               <BlogRoll />
             </div>
