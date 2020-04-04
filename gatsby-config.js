d8f8be897d4d4dbca9f611a706bb78e8271e55da
kajirikajiri <kajirikajiri@gmail.com>
2020-04-04 18:17:13 +0900

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,7 @@ module.exports = {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
+          "gatsby-remark-embed-youtube",
           {
             resolve: 'gatsby-remark-relative-images',
             options: {
